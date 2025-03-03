1e62eda000609b8aa862a28844934f89da048eb0
Nicolas Buquet <nbuquet@buquet-net.com>
2025-03-03 16:55:42 +0100

    Make Compound Package reference local cloned Compound-design-token repository

--- Package.swift
+++ Package.swift
@@ -9,8 +9,7 @@ let package = Package(
         .library(name: "Compound", targets: ["Compound"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tchapgouv/compound-design-tokens", branch: "main"),
-//        .package(path: "../compound-design-tokens"),
+        .package(path: "../compound-design-tokens"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "1.3.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "6.2.0"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.18.3")
