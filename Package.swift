2787f13fbfd20d22ba311726e5005ada4774a99a
Nicolas Buquet <nbuquet@buquet-net.com>
2025-04-22 15:20:35 +0200

    Use compound-design-token branch `angelo`

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .library(name: "Compound", targets: ["Compound"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tchapgouv/compound-design-tokens", branch: "main"),
+        .package(url: "https://github.com/tchapgouv/compound-design-tokens", branch: "angelo"),
+//        .package(path: "../compound-design-tokens"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "1.3.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "6.2.0"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.18.3")
